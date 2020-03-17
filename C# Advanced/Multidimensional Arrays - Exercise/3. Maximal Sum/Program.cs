b32ecdeae810b73d9fd5495f835b994c8ac50715
Adem <ademcran4aliev@abv.bg>
2020-03-17 23:28:09 +0200

    Fixing

--- C# Advanced/Multidimensional Arrays - Exercise/3. Maximal Sum/Program.cs
+++ C# Advanced/Multidimensional Arrays - Exercise/3. Maximal Sum/Program.cs
@@ -7,12 +7,12 @@ namespace _3._Maximal_Sum
     {
         static void Main(string[] args)
         {
-            var input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var input = Console.ReadLine().Split(new[] { ' ' },StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
             var matrix = new int[input[0], input[1]];
 
             for (int i = 0; i < input[0]; i++)
             {
-                var input2 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                var input2 = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
                 for (int j = 0; j < input[1]; j++)
                 {
                     matrix[i, j] = input2[j];
@@ -28,8 +28,8 @@ namespace _3._Maximal_Sum
                     if (sum > max)
                     {
                         res[0] = matrix[i, j];
-                        res[2] = matrix[i , j + 1];
-                        res[1] = matrix[i , j + 2];
+                        res[1] = matrix[i , j + 1];
+                        res[2] = matrix[i , j + 2];
                         res[3] = matrix[i + 1, j];
                         res[4] = matrix[i + 1, j + 1];
                         res[5] = matrix[i + 1, j + 2];
