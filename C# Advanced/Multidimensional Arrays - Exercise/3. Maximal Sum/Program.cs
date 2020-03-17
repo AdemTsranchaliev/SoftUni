4d63e89e60215a38dfdfeb29633b3adc8d7b389f
Adem <ademcran4aliev@abv.bg>
2020-03-17 23:24:59 +0200

    Fixed Maximal sum

--- C# Advanced/Multidimensional Arrays - Exercise/3. Maximal Sum/Program.cs
+++ C# Advanced/Multidimensional Arrays - Exercise/3. Maximal Sum/Program.cs
@@ -12,7 +12,7 @@ namespace _3._Maximal_Sum
 
             for (int i = 0; i < input[0]; i++)
             {
-                var input2 = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+                var input2 = Console.ReadLine().Split().Select(int.Parse).ToArray();
                 for (int j = 0; j < input[1]; j++)
                 {
                     matrix[i, j] = input2[j];
@@ -20,25 +20,33 @@ namespace _3._Maximal_Sum
             }
             int max = int.MinValue;
             var res = new int[9];
-            for (int i = 0; i < input[0] - 1; i++)
+            for (int i = 0; i < input[0] - 2; i++)
             {
-                for (int j = 0; j < input[1] - 1; j++)
+                for (int j = 0; j < input[1] - 2; j++)
                 {
-                    int sum = matrix[i, j] + matrix[i + 1, j] + matrix[i, j + 1] + matrix[i + 1, j + 1];
+                    int sum = matrix[i, j] + matrix[i + 1, j] + matrix[i, j + 1] + matrix[i + 1, j + 1] + matrix[i + 2, j + 2] + matrix[i, j + 2] + matrix[i + 2, j ] + matrix[i + 1, j + 2] + matrix[i + 2, j + 1];
                     if (sum > max)
                     {
                         res[0] = matrix[i, j];
-                        res[2] = matrix[i + 1, j];
-                        res[1] = matrix[i, j + 1];
-                        res[3] = matrix[i + 1, j + 1];
+                        res[2] = matrix[i , j + 1];
+                        res[1] = matrix[i , j + 2];
+                        res[3] = matrix[i + 1, j];
+                        res[4] = matrix[i + 1, j + 1];
+                        res[5] = matrix[i + 1, j + 2];
+                        res[6] = matrix[i+2 , j ];
+                        res[7] = matrix[i + 2, j + 1];
+                        res[8] = matrix[i + 2, j + 2];
+
+
                         max = sum;
                     }
                 }
             }
-            Console.WriteLine($"{res[0]} {res[1]}");
-            Console.WriteLine($"{res[2]} {res[3]}");
+            Console.WriteLine("Sum = "+max);
 
-            Console.WriteLine(max);
+            Console.WriteLine($"{res[0]} {res[1]} {res[2]}");
+            Console.WriteLine($"{res[3]} {res[4]} {res[5]}");
+            Console.WriteLine($"{res[6]} {res[7]} {res[8]}");
         }
     }
 }
