b243b578328aa42dead030ce62322882e3f1c128
Adem <ademcran4aliev@abv.bg>
2020-03-26 00:45:44 +0200

    Added Defining Classes - Lab.

---
+++ C# Advanced/Defining Classes - Lab/2. Car Extension/Car.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+        private double fuelQuantity;
+        private double fuelConsumption;
+
+        public string Make
+        {
+            get
+            {
+                return this.make;
+            }
+            set
+            {
+                this.make = value;
+            }
+        }
+        public string Model
+        {
+            get
+            {
+                return this.model;
+            }
+            set
+            {
+                this.model = value;
+            }
+        }
+        public int Year
+        {
+            get
+            {
+                return this.year;
+            }
+            set
+            {
+                this.year = value;
+            }
+        }
+        public double FuelQuantity
+        {
+            get
+            {
+                return this.fuelQuantity;
+            }
+            set
+            {
+                this.fuelQuantity = value;
+            }
+        }
+        public double FuelConsumption
+        {
+            get
+            {
+                return this.fuelConsumption;
+            }
+            set
+            {
+                this.fuelConsumption = value;
+            }
+        }
+
+        public void Drive(double distance)
+        {
+            double expenceFuel = FuelConsumption * distance / 100;
+
+            if (expenceFuel > FuelQuantity)
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+            else
+            {
+                FuelQuantity -= distance / 100 * FuelConsumption;
+            }
+        }
+        public string WhoAmI()
+        {
+
+            StringBuilder str = new StringBuilder();
+            str.AppendLine($"Make: {this.Make}");
+            str.AppendLine($"Model: {this.Model}");
+            str.AppendLine($"Year: {this.Year}");
+            str.Append($"Fuel: {this.FuelQuantity:F2}L");
+
+            return str.ToString();
+        }
+    }
+}
---
+++ C# Advanced/Defining Classes - Lab/3. Car Constructors/Car.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+        private double fuelQuantity;
+        private double fuelConsumption;
+
+
+        public Car()
+        {
+            this.Make ="VW";
+            this.Model ="Golf";
+            this.Year =2025;
+            this.FuelConsumption=10;
+            this.FuelQuantity=200;
+        }
+
+        public Car(string make,string model,int year):this()
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
+        }
+        public Car(string make, string model, int year, double fuelQuantity,double fuelConsumption):this(make,model,year)
+        {
+            this.FuelConsumption = fuelConsumption;
+            this.FuelQuantity = fuelQuantity;
+        }
+
+
+        public string Make
+        {
+            get
+            {
+                return this.make;
+            }
+            set
+            {
+                this.make = value;
+            }
+        }
+        public string Model
+        {
+            get
+            {
+                return this.model;
+            }
+            set
+            {
+                this.model = value;
+            }
+        }
+        public int Year
+        {
+            get
+            {
+                return this.year;
+            }
+            set
+            {
+                this.year = value;
+            }
+        }
+        public double FuelQuantity
+        {
+            get
+            {
+                return this.fuelQuantity;
+            }
+            set
+            {
+                this.fuelQuantity = value;
+            }
+        }
+        public double FuelConsumption
+        {
+            get
+            {
+                return this.fuelConsumption;
+            }
+            set
+            {
+                this.fuelConsumption = value;
+            }
+        }
+        public void Drive(double distance)
+        {
+            double expenceFuel = FuelConsumption * distance / 100;
+
+            if (expenceFuel > FuelQuantity)
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+            else
+            {
+                FuelQuantity -= distance / 100 * FuelConsumption;
+            }
+        }
+        public string WhoAmI()
+        {
+
+            StringBuilder str = new StringBuilder();
+            str.AppendLine($"Make: {this.Make}");
+            str.AppendLine($"Model: {this.Model}");
+            str.AppendLine($"Year: {this.Year}");
+            str.Append($"Fuel: {this.FuelQuantity:F2}L");
+
+            return str.ToString();
+        }
+
+
+
+    }
+}
